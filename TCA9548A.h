c175bfdb49c74809d83afa57f2d932278a3dd2c2
Giulio Moro <giuliomoro@yahoo.it>
2021-05-08 15:07:45 +0000

    Added support for TCA9548A I2C multiplexer to select among several screens of the same type

---
+++ TCA9548A.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <I2c.h>
+
+class TCA9548A : public I2c
+{
+public:
+	TCA9548A() {i2C_file = -1;};
+	TCA9548A(int bus, int address)
+	{
+		int ret = initI2C_RW(bus, address, -1);
+		if(ret || select(-1)) // disable all channels and at the same time verify address is valid
+			throw std::runtime_error("Unable to open TCA9548A. Ensure the multiplexer is connected"
+			"and the bus and address are correct.");
+	}
+	int readI2C() override {return 0;};
+	int select(int channel)
+	{
+		i2c_char_t byte = channel < 0 || channel >= 8 ? 0 : 1 << channel;
+		return (sizeof(byte) != write(i2C_file, &byte, sizeof(byte)));
+	}
+};
--- main.cpp
+++ main.cpp
@@ -94,10 +94,27 @@ times a second.
 #include <unistd.h>
 #include "u8g2/cppsrc/U8g2lib.h"
 
-const unsigned int SH1106_addr = 0x3c;
+// #define I2C_MUX // allow I2C multiplexing to select different target displays (assuming it agrees with gTargetMode)
+const unsigned int gOledAddress = 0x3c;
 U8G2_SH1106_128X64_NONAME_F_HW_I2C_LINUX u8g2(U8G2_R0);
+int gLocalPort = 7562; //port for incoming OSC messages
+
+#ifdef I2C_MUX
+#include "TCA9548A.h"
+const unsigned int gMuxAddress = 0x70;
+TCA9548A gTca;
+const unsigned int gI2cBus = 1; // TODO: this is only used by the muxer at the moment
+#endif // I2C_MUX
+
+/// Determines how to select which display a message is targeted to:
+typedef enum {
+	kTargetSingle, ///< Single target (one display).
+	kTargetEach, ///< The first argument to each message is an index corresponding to the target display
+	kTargetStateful, ///< Send a message to /target <float> to select which is the active display that all subsequent messages will be sent to
+} TargetMode;
+
+TargetMode gTargetMode = kTargetSingle; // can be changed with /targetMode
 OscReceiver oscReceiver;
-int localPort = 7562; //port for incoming OSC messages
 int gStop = 0;
 
 // Handle Ctrl-C by requesting that the audio rendering stop
@@ -105,95 +122,196 @@ void interrupt_handler(int var)
 {
 	gStop = true;
 }
+template <typename T>
+static bool popNumber(oscpkt::Message::ArgReader& args, T& val)
+{
+	if (args.isInt32()) {
+		int32_t i;
+		args.popInt32(i);
+		val = i;
+	} else if (args.isInt64()) {
+		int64_t i;
+		args.popInt64(i);
+	} else if(args.isFloat()) {
+		float f;
+		args.popFloat(f);
+		val = f;
+	} else if(args.isDouble()) {
+		double d;
+		args.popDouble(d);
+		val = d;
+	} else if (args.isBool()) {
+		bool b;
+		args.popBool(b);
+		val = b;
+	} else {
+		return false;
+	}
+	return true;
+}
 
-int parseMessage(oscpkt::Message msg, void* arg)
+int parseMessage(oscpkt::Message msg, void*)
 {
 	u8g2.clearBuffer();
 
 	int displayWidth = u8g2.getDisplayWidth();
 	int displayHeight = u8g2.getDisplayHeight();
-	std::string trString;
 	float param1Value;
 	float param2Value;
 	float param3Value;
-	std::string text1;
-	std::string text2;
-	std::string text3;
+	oscpkt::Message::ArgReader args = msg.arg();
+	enum {
+		kOk = 0,
+		kUnmatchedPattern,
+		kWrongArguments,
+		kInvalidMode,
+		kOutOfRange,
+	} error = kOk;
+	bool stateMessage = false;
+	// check state (non-display) messages first
+	if (msg.match("/target")) {
+		stateMessage = true;
+		if(kTargetStateful != gTargetMode) {
+			fprintf(stderr, "Target mode is not stateful, so /target messages are ignored\n");
+			error = kInvalidMode;
+		} else {
+			int target;
+			bool found = popNumber(args, target);
+			if(found && args.isOkNoMoreArgs()) {
+#ifdef I2C_MUX
+				gTca.select(target);
+#else // I2C_MUX
+				fprintf(stderr, "Multi-display mode is only available via I2C_MUX. /target %d ignored.\n", target);
+#endif // I2C_MUX
+			} else {
+				fprintf(stderr, "Argument to /target should be numeric (int or float)\n");
+				error = kWrongArguments;
+			}
+		}
+	} else if (msg.match("/targetMode")) {
+		stateMessage = true;
+		int mode;
+		bool found = popNumber(args, mode);
+		if(!found)
+			error = kWrongArguments;
+		else {
+			if(mode != kTargetSingle && mode != kTargetStateful && mode != kTargetEach)
+				error = kOutOfRange;
+			else {
+				gTargetMode = (TargetMode)mode;
+				printf("Target mode: %d\n", mode);
+			}
+		}
+	}
+	if(!stateMessage && kTargetEach == gTargetMode)
+	{
+		// if we are in kTargetEach and the message is for a display, we need to peel off the
+		// first argument (which denotes the target display) before processing the message
+		int target;
+		bool found = popNumber(args, target);
+		if(!found) {
+			fprintf(stderr, "Target mode is \"Each\", therefore the first argument should be an int or float specifying the target display\n");
+			error = kWrongArguments;
+		}
+		else {
+#ifdef I2C_MUX
+			gTca.select(target);
+#else // I2C_MUX
+			fprintf(stderr, "Multi-display mode is only available via I2C_MUX. Target %d ignored.\n", target);
+#endif // I2C_MUX
+		}
+	}
 
-	if (msg.match("/osc-test").isOkNoMoreArgs())
+	// code below MUST use msg.match() to check patterns and args.pop... or args.is ... to check message content.
+	// this way, anything popped above (if we are in kTargetEach mode), won't be re-used below
+	if(error || stateMessage) {
+		// nothing to do here, just avoid matching any of the others
+	} else if (msg.match("/osc-test"))
 	{
-		printf("received /osc-test\n");
-		u8g2.setFont(u8g2_font_ncenB08_tr);
-		u8g2.setFontRefHeightText();
-		u8g2.drawStr(0, displayHeight * 0.5, "OSC TEST SUCCESS!");
-		u8g2.sendBuffer();
-	} else if (msg.match("/number").popStr(trString).isOkNoMoreArgs())
+		if(!args.isOkNoMoreArgs())
+			error = kWrongArguments;
+		else {
+			printf("received /osc-test\n");
+			u8g2.setFont(u8g2_font_ncenB08_tr);
+			u8g2.setFontRefHeightText();
+			u8g2.drawStr(0, displayHeight * 0.5, "OSC TEST SUCCESS!");
+		}
+	} else if (msg.match("/number"))
 	{
-		const char *ctrStr = trString.c_str();
-		printf("received /number %s\n", ctrStr);
-		u8g2.setFont(u8g2_font_logisoso62_tn);
-		u8g2.drawStr(0, 0, ctrStr);
-		u8g2.sendBuffer();
-	} else if (msg.match("/display-text").popStr(text1).popStr(text2).popStr(text3).isOkNoMoreArgs())
+		std::string numberString;
+		if(!args.popStr(numberString).isOkNoMoreArgs())
+			error = kWrongArguments;
+		else
+		{
+			const char *numStr = numberString.c_str();
+			printf("received /number %s\n", numStr);
+			u8g2.setFont(u8g2_font_logisoso62_tn);
+			u8g2.drawUTF8(0, 0, numStr);
+		}
+	} else if (msg.match("/display-text"))
 	{
-		const char *ctrStr1 = text1.c_str();
-		const char *ctrStr2 = text2.c_str();
-		const char *ctrStr3 = text3.c_str();
-		printf("received /display-text string %s %s %s\n", ctrStr1, ctrStr2, ctrStr3);
-		u8g2.setFont(u8g2_font_4x6_tf);
-		u8g2.setFontRefHeightText();
-		u8g2.drawUTF8(displayWidth * 0.5, displayHeight * 0.25, ctrStr1);
-		u8g2.drawUTF8(displayWidth * 0.5, displayHeight * 0.5, ctrStr2);
-		u8g2.drawUTF8(displayWidth * 0.5, displayHeight * 0.75, ctrStr3);
-		u8g2.sendBuffer();
-	} else 	if (msg.match("/parameters").popFloat(param1Value).popFloat(param2Value).popFloat(param3Value).isOkNoMoreArgs())
+		std::string text1;
+		std::string text2;
+		std::string text3;
+		if(!args.popStr(text1).popStr(text2).popStr(text3).isOkNoMoreArgs())
+			error = kWrongArguments;
+		else {
+			const char *ctrStr1 = text1.c_str();
+			const char *ctrStr2 = text2.c_str();
+			const char *ctrStr3 = text3.c_str();
+			printf("received /display-text string %s %s %s\n", ctrStr1, ctrStr2, ctrStr3);
+			u8g2.setFont(u8g2_font_4x6_tf);
+			u8g2.setFontRefHeightText();
+			u8g2.drawUTF8(displayWidth * 0.5, displayHeight * 0.25, ctrStr1);
+			u8g2.drawUTF8(displayWidth * 0.5, displayHeight * 0.5, ctrStr2);
+			u8g2.drawUTF8(displayWidth * 0.5, displayHeight * 0.75, ctrStr3);
+		}
+	} else if (msg.match("/parameters"))
 	{
-		printf("received /parameters float %f float %f float %f\n", param1Value, param2Value, param3Value);
-		u8g2.setFont(u8g2_font_4x6_tf);
-		u8g2.setFontRefHeightText();
-		u8g2.drawStr(0, 0, "PARAMETER 1:");
-		u8g2.drawBox(0, 10, u8g2.getDisplayWidth() * param1Value, 10);
-		u8g2.drawStr(0, 22, "PARAMETER 2:");
-		u8g2.drawBox(0, 32, u8g2.getDisplayWidth() * param2Value, 10);
-		u8g2.drawStr(0, 44, "PARAMETER 3:");
-		u8g2.drawBox(0, 54, u8g2.getDisplayWidth() * param3Value, 10);
-		u8g2.sendBuffer();
-	} else 	if (msg.match("/lfos").popFloat(param1Value).popFloat(param2Value).popFloat(param3Value).isOkNoMoreArgs())
+		if(!args.popFloat(param1Value).popFloat(param2Value).popFloat(param3Value).isOkNoMoreArgs())
+			error = kWrongArguments;
+		else {
+			printf("received /parameters float %f float %f float %f\n", param1Value, param2Value, param3Value);
+			u8g2.setFont(u8g2_font_4x6_tf);
+			u8g2.setFontRefHeightText();
+			u8g2.drawStr(0, 0, "PARAMETER 1:");
+			u8g2.drawBox(0, 10, u8g2.getDisplayWidth() * param1Value, 10);
+			u8g2.drawStr(0, 22, "PARAMETER 2:");
+			u8g2.drawBox(0, 32, u8g2.getDisplayWidth() * param2Value, 10);
+			u8g2.drawStr(0, 44, "PARAMETER 3:");
+			u8g2.drawBox(0, 54, u8g2.getDisplayWidth() * param3Value, 10);
+		}
+	} else if(msg.match("/lfos"))
 	{
+		if(!args.popFloat(param1Value).popFloat(param2Value).popFloat(param3Value).isOkNoMoreArgs())
+			error = kWrongArguments;
 		printf("received /lfos float %f float %f float %f\n", param1Value, param2Value, param3Value);
 		u8g2.drawEllipse(displayWidth * 0.2, displayHeight * 0.5, 10, displayHeight * 0.5 * param1Value);
 		u8g2.drawEllipse(displayWidth * 0.5, displayHeight * 0.5, 10, displayHeight * 0.5 * param2Value);
 		u8g2.drawEllipse(displayWidth * 0.8, displayHeight * 0.5, 10, displayHeight * 0.5 * param3Value);
 		u8g2.drawHLine(0, displayHeight * 0.5, displayWidth);
-		u8g2.sendBuffer();
 	} else if (msg.match("/waveform"))
 	{
-		oscpkt::Message::ArgReader args(msg);
-		if(args)
+		const unsigned int nValues = args.nbArgRemaining();
+		float values[nValues];
+		for(unsigned int n = 0; n < nValues; ++n)
 		{
-			const unsigned int nValues = args.nbArgRemaining();
-			float values[nValues];
-			for(unsigned int n = 0; n < nValues; ++n)
+			if(args.isFloat())
 			{
-				if(args.isFloat())
-				{
-					args.popFloat(values[n]);
-				} else if(args.isInt32()) {
-					int i;
-					args.popInt32(i);
-					values[n] = i;
-				} else {
-					fprintf(stderr, "Wrong type at argument %d\n", n);
-					return -1;
-				}
+				args.popFloat(values[n]);
+			} else if(args.isInt32()) {
+				int i;
+				args.popInt32(i);
+				values[n] = i;
+			} else {
+				error = kWrongArguments;
 			}
-			// now tenValues contains the 10 values.
-			// prepare a bitmap:
-			printf("received /waveform floats:");
-			for(unsigned int n = 0; n < 10; ++n)
-			printf("%f ", values[n]);
-			printf("\n");
+		}
+		if(!error)
+		{
+			printf("received /waveform with %d values\n", nValues);
 
+			// prepare a bitmap:
 			for(unsigned int x = 0; x < displayWidth; ++x)
 			{
 				// we interpret each value as the vertical displacement and
@@ -204,20 +322,51 @@ int parseMessage(oscpkt::Message msg, void* arg)
 			}
 		}
 	} else
+		error = kUnmatchedPattern;
+	if(error)
 	{
-		printf("unhandled message to: %s \n", msg.addressPattern().c_str());
+		std::string str;
+		switch(error){
+			case kUnmatchedPattern:
+				str = "no matching pattern available\n";
+				break;
+			case kWrongArguments:
+				str = "unexpected types and/or length\n";
+				break;
+			case kInvalidMode:
+				str = "invalid target mode\n";
+				break;
+			case kOutOfRange:
+				str = "argument(s) value(s) out of range\n";
+				break;
+			case kOk:
+				str = "";
+				break;
+		}
+		fprintf(stderr, "An error occurred with message to: %s: %s\n", msg.addressPattern().c_str(), str.c_str());
+		return 1;
+	} else
+	{
+		if(!stateMessage)
+			u8g2.sendBuffer();
 	}
-	u8g2.sendBuffer();
 	return 0;
 }
 
 int main(int main_argc, char *main_argv[])
 {
+#ifdef I2C_MUX
+	if(gTca.initI2C_RW(gI2cBus, gMuxAddress, -1) || gTca.select(-1))
+	{
+		fprintf(stderr, "Unable to initialise the TCA9548A multiplexer. Are the address and bus correct?\n");
+		return 1;
+	}
+#endif // I2C_MUX
 	// Set up interrupt handler to catch Control-C and SIGTERM
 	signal(SIGINT, interrupt_handler);
 	signal(SIGTERM, interrupt_handler);
 
-	u8g2.setI2CAddress(SH1106_addr);
+	u8g2.setI2CAddress(gOledAddress);
 	u8g2.initDisplay();
 	u8g2.setPowerSave(0);
 	u8g2.clearBuffer();
@@ -232,10 +381,10 @@ int main(int main_argc, char *main_argv[])
 	u8g2.sendBuffer();
 
 	// OSC
-	oscReceiver.setup(localPort, parseMessage);
+	oscReceiver.setup(gLocalPort, parseMessage);
 	while(!gStop)
 	{
 		usleep(100000);
 	}
-	return false;
+	return 0;
 }
